c4cc6f81d8906406460e0bb4f265759a54ef4792
callmeAngga <wongfromindo@gmail.com>
2024-06-13 11:00:05 +0700

    Frontend #2

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 import streamlit as st
+from ultralytics import YOLO
 import cv2
 import base64
 import numpy as np
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode, RTCConfiguration
 from streamlit_option_menu import option_menu
+import threading
 
 # Konfigurasi WebRTC
 RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    layout="wide",
+    page_title="Traffuck"
+)
+
+# model = YOLO('best10.pt')
+
+class_names = [
+    "Speed limit (20km/h)", "Speed limit (30km/h)", "Speed limit (50km/h)", "Speed limit (60km/h)",
+    "Speed limit (70km/h)", "Speed limit (80km/h)", "End of speed limit (80km/h)", "Speed limit (100km/h)",
+    "Speed limit (120km/h)", "No passing", "No passing for vehicles over 3.5 metric tons", 
+    "Right-of-way at the next intersection", "Priority road", "Yield", "Stop", "No vehicles", 
+    "Vehicles over 3.5 metric tons prohibited", "No entry", "General caution", "Dangerous curve to the left", 
+    "Dangerous curve to the right", "Double curve", "Bumpy road", "Slippery road", "Road narrows on the right", 
+    "Road work", "Traffic signals", "Pedestrians", "Children crossing", "Bicycles crossing", 
+    "Beware of ice/snow", "Wild animals crossing", "End of all speed and passing limits", "Turn right ahead", 
+    "Turn left ahead", "Ahead only", "Go straight or right", "Go straight or left", "Keep right", "Keep left", 
+    "Roundabout mandatory", "End of no passing", "End of no passing by vehicles over 3.5 metric tons"
+]
 
 selected = option_menu(
     menu_title=None,  
